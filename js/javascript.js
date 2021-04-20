db3fc179030c720359e4d1911daf346e870a8813
YNCatari <77997513+YNCatari@users.noreply.github.com>
2021-04-20 08:04:40 -0500

    diseño de sitio web

---
+++ js/javascript.js
@@ -0,0 +1,10 @@
+function Bienvenidos() 
+{ 
+   
+    alert("Su Nombre Es:"+document.getElementById("txtNombre").value);
+    alert("Su Apellido Es:"+document.getElementById("txtApellido").value);
+    alert("Su Direccion Es:"+document.getElementById("txtDireccion").value);
+    alert("Su Telefono Es"+document.getElementById("txtTelefono").value);
+}
+	
+		
